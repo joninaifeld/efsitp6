50279b40f9699a0decfde5d5230a9e20892fffbb
Android Studio <devandroid@ort>
2026-05-26 10:40:23 -0300

    weights y hovers

--- src/components/PostModal.tsx
+++ src/components/PostModal.tsx
@@ -35,20 +35,20 @@ function PostModal({ data, setDisplayingPost }: PostProps) {
                     <header>
                         {data.userImage && <img src={data.userImage} alt="User profile" className="postModalProfile" />}
                         <div className="title">
-                            <h2>{data.username}</h2>
-                            <p>{data.year}</p>
+                            <h2 className="username">{data.username}</h2>
+                            <p className="year">{data.year}</p>
                         </div>
                     </header>
                     <main>
                         { data.comments && data.comments.length > 0 
                             ? data.comments.map((comment) => comment.text) 
-                            : <p>No comments yet. Be the first to comment!</p>
+                            : <p className="noComments">No comments yet. Be the first to comment!</p>
                         }
                     </main>
                     <footer>
                         <div className="mainInfo">
                             <div className="postModalActions">
-                                <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-heart">
+                                <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-heart likeButton">
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                     <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
                                 </svg>
